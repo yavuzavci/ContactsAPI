47b3b948bfddca99d277133051e9e417733cf259
yavuzavci <yavuzavci@gmail.com>
2022-02-17 15:29:56 +0300

    Add database connection

---
+++ ContactsAPI/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ContactsAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
