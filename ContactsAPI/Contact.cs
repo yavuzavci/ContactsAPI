13707407a6e66d7ab2edeb26cd3d99d76144975c
yavuzavci <yavuzavci@gmail.com>
2022-02-17 09:54:43 +0300

    Add project files.

---
+++ ContactsAPI/Contact.cs
@@ -0,0 +1,13 @@
+﻿namespace ContactsAPI
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string Company { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+    }
+}
---
+++ ContactsAPI/Controllers/ContactController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+    }
+}
